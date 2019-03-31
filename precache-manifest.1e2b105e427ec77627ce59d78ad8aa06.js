c59b43d2004dcb8d895ddd3ea38b011df3c9a4f0
Karol Makara <karol_makara17@wp.pl>
2019-03-31 18:02:56 +0200

    Updates

---
+++ precache-manifest.1e2b105e427ec77627ce59d78ad8aa06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "33da2b9cb7a60e1e9d92",
+    "url": "/pokedex/static/js/main.27bf2686.chunk.js"
+  },
+  {
+    "revision": "9213c98268ec5f1e960c",
+    "url": "/pokedex/static/js/2.093fd198.chunk.js"
+  },
+  {
+    "revision": "33da2b9cb7a60e1e9d92",
+    "url": "/pokedex/static/css/main.67cdc890.chunk.css"
+  },
+  {
+    "revision": "6458ae038a455d3a9da3e6d19e9bbc48",
+    "url": "/pokedex/index.html"
+  }
+];
